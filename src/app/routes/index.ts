338f9772d9d3a648abbca537a78f21ee5049dafe
Mohammad Arif <programmermdarif@gmail.com>
2024-06-13 12:48:44 +0600

    Added car interface and model

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,9 +1,6 @@
 import { Router } from 'express';
-import { StudentRoutes } from '../modules/student/student.route';
+
 import { UserRoutes } from '../modules/user/user.route';
-import { AcademicSemesterRoute } from '../modules/academicSemister/academiSemester.route';
-import { AcademicFacultyRoute } from '../modules/academicFaculty/academicFaculty.route';
-import { AcademicDepartmentRoute } from '../modules/academicDepartment/academicDepartment.router';
 
 const router = Router();
 
@@ -11,22 +8,6 @@ const moduleRoutes = [
   {
     path: '/users',
     route: UserRoutes,
-  },
-  {
-    path: '/students',
-    route: StudentRoutes,
-  },
-  {
-    path: '/academic-semesters',
-    route: AcademicSemesterRoute,
-  },
-  {
-    path:'/academic-faculty',
-    route:AcademicFacultyRoute
-  },
-  {
-    path:'/academic-department',
-    route:AcademicDepartmentRoute
   },
 ];
 
