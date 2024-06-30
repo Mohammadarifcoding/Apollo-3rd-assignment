673d628cf96d90b38a88369e80ccda06bf4bf56a
Mohammad Arif <programmermdarif@gmail.com>
2024-06-30 23:27:37 +0600

    Done the formatting

--- src/app/modules/car/car.controller.ts
+++ src/app/modules/car/car.controller.ts
@@ -1,12 +1,11 @@
 import httpStatus from 'http-status';
 
-import {  RequestHandler } from 'express';
+import { RequestHandler } from 'express';
 import sendResponse from '../../utils/sendResponse';
 import catchAsync from '../../utils/catchAsync';
 import { CarServices } from './car.service';
 
 const createCar: RequestHandler = catchAsync(async (req, res) => {
-
   const result = await CarServices.CreateCarIntoDb(req.body);
 
   sendResponse(res, {
@@ -18,18 +17,28 @@ const createCar: RequestHandler = catchAsync(async (req, res) => {
 });
 
 const GetCar: RequestHandler = catchAsync(async (req, res) => {
+  const result = await CarServices.GetAllCarsFromDb();
 
-    const result = await CarServices.GetAllCarsFromDb();
-  
-    sendResponse(res, {
-      success: true,
-      statusCode: httpStatus.OK,
-      message: "Cars retrieved successfully",
-      data: result,
-    });
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: 'Cars retrieved successfully',
+    data: result,
   });
+});
 
+const GetCarById:RequestHandler = catchAsync(async(req,res)=>{
+    const {id} = req.params
+    const result = await CarServices.GetCarBasedId(id)
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message:  "A Car retrieved successfully",
+        data: result,
+      });
+})
 
 export const CarControllers = {
-  createCar,GetCar
+  createCar,
+  GetCar,GetCarById
 };
--- src/app/modules/car/car.service.ts
+++ src/app/modules/car/car.service.ts
@@ -1,16 +1,28 @@
-import { TCar } from "./car.interface";
-import { CarModel } from "./car.model";
+import httpStatus from 'http-status';
+import AppError from '../../errors/AppError';
+import { TCar } from './car.interface';
+import { CarModel } from './car.model';
 
-const CreateCarIntoDb = async(payload:TCar)=>{
- const result = await CarModel.create(payload)
- const savedResult = await CarModel.findById(result._id)
- return savedResult
-}
+const CreateCarIntoDb = async (payload: TCar) => {
+  const result = await CarModel.create(payload);
+  const savedResult = await CarModel.findById(result._id);
+  return savedResult;
+};
 
-const GetAllCarsFromDb = async()=>{
-    const result = await CarModel.find()
-    return result
-}
+const GetAllCarsFromDb = async () => {
+  const result = await CarModel.find();
+  return result;
+};
+
+const GetCarBasedId = async (id: string) => {
+  const result = await CarModel.findById(id);
+  if (!result) {
+    throw new AppError(httpStatus.NOT_FOUND, "Didn't found any Car");
+  }
+  return result;
+};
 export const CarServices = {
-    CreateCarIntoDb,GetAllCarsFromDb
-}
+  CreateCarIntoDb,
+  GetCarBasedId,
+  GetAllCarsFromDb,
+};
